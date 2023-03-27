8aa35370744fce4d8e78936bbfd95e75c7896d0c
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-03-27 18:28:49 +0200

    update

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -12,31 +12,33 @@ const Footrer = () => {
           <h5>GPT-5</h5>
           <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
         </div>
-        <div className="footer__link-wrapper">
-          <FooterLinks
-            span="Links"
-            link1="Overons"
-            link2="Social Media"
-            link3="Counters"
-            link4="Contact"
-          />
-        </div>
-        <div className="footer__link-wrapper">
-          <FooterLinks
-            span="Company"
-            link1="Terms & Conditions"
-            link2="Privacy Policy"
-            link3="Gilroy-Medium"
-            link4=""
-          />
-        </div>
-        <div className="footer__link-wrapper">
-          <FooterLinks
-            span="Get in touch"
-            link1="mohammad zeyghami"
-            link2="+98-09189563646"
-            link3="mohammadzeighami192@g"
-          />
+        <div className="footer_bottom">
+          <div className="footer__link-wrapper">
+            <FooterLinks
+              span="Links"
+              link1="Overons"
+              link2="Social Media"
+              link3="Counters"
+              link4="Contact"
+            />
+          </div>
+          <div className="footer__link-wrapper">
+            <FooterLinks
+              span="Company"
+              link1="Terms & Conditions"
+              link2="Privacy Policy"
+              link3="Gilroy-Medium"
+              link4=""
+            />
+          </div>
+          <div className="footer__link-wrapper">
+            <FooterLinks
+              span="Get in touch"
+              link1="mohammad zeyghami"
+              link2="+98-09189563646"
+              link3="mohammadzeighami192@g"
+            />
+          </div>
         </div>
       </div>
     </div>
